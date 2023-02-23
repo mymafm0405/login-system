d3173efb2feee4e054e5cb8a4d75b082c9c02e94
Mahmoud <mymafm0405@gmail.com>
2023-02-23 10:33:50 +0300

    update guard

--- src/app/components/auth/auth-guard.service.ts
+++ src/app/components/auth/auth-guard.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
 } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -9,12 +10,20 @@ import { LoginService } from 'src/app/shared/login.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuardService implements CanActivate {
-  constructor(private loginServ: LoginService) {}
+  constructor(private loginServ: LoginService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    return !!this.loginServ.user;
+    const promise = new Promise<boolean>((resolve, reject) => {
+        if (this.loginServ.user.value) {
+            resolve (true);
+        } else {
+            resolve(false)
+            this.router.navigate(['/'])
+        }
+    })
+    return promise;
   }
 }
